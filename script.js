ff864b7cb8c6a7d1a0739bcb617c54a56b1ef37e
casa <boudetch@gmail.com>
2011-11-04 16:11:45 +0100

    add div bar on page

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert('a')
+/*
+	Chooply 2011
+	Main part 
+*/
+
+$(document).ready(function(){
+
+
+	//add div on body of the page to create the bar
+	$('body').append('<div id="chooply_bar">My Bar</div>');
+})
