0ffe8c67fb7c184ccc82b6a45e7a37b68cea9576
casa <boudetch@gmail.com>
2011-11-04 16:24:57 +0100

    frequence of each words

--- script.js
+++ script.js
@@ -5,18 +5,46 @@
 
 $(document).ready(function(){
 
+	var score = new Array();	//score of each words
+	
+
+	//fonction to count the frequence of each words
+	function calcul_score(words)
+	{
+		for(i=0; i<words.length; i++)
+		{
+		 	word = words[i];
+		 	
+		 	if(score[word])
+		 	{
+		 		score[word] += 1;
+		 	}
+		 	else
+		 	{
+		 		score[word] = 1;
+		 	}
+		 }	
+	}
+
+
 
 	//add div on body of the page to create the bar
 	$('body').append('<div id="chooply_bar">My Bar</div>');
 	
-	//get text of the page
-	//$('#chooply_bar').append($('body').text() +"<hr>");
+	
+	//get words of the first div
 	var words = $('body>div').text().match(/\b([a-zA-Zéèàê]{4,14})\b/g);
 	
 	
-	for(key in words)
+	calcul_score(words);
+	
+	
+	
+	//debug
+	for(key in score)
 	{
-		$('#chooply_bar').append(words[key] +"<br>");
+		$('#chooply_bar').append(key + " : "+ score[key] +"<br>");
 	}
+	
 	
 })
